861cc21e7b5e55ac43903304eb7255e5ae78cbb7
daryn <darynnbrown@gmail.com>
2022-06-01 04:26:10 -0500

    added icons

---
+++ popup.js
@@ -0,0 +1,28 @@
+// adding a new bookmark row to the popup
+const addNewBookmark = () => {};
+
+const viewBookmarks = () => {};
+
+const onPlay = e => {};
+
+const onDelete = e => {};
+
+const setBookmarkAttributes =  () => {};
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const activeTab = await getActiveTabURL();
+    const queryParameters = activeTab.url.split("?")[1];
+    const  urlParameters = new URLSearchParams(queryParameters);
+
+    const currentVideo = urlParameters.get("v");
+
+    if (activeTab.url.includes("youtube.com/watch") && currentVideo){
+        chrome.storage.sync.get([currentVideo], (data) => {
+            const currentVideoBookmarks = data[currentVideo] ? JSON.parse(data[currentVideo]): [];
+            
+            //viewBookmarks
+        })
+    } else {
+        
+    }
+});
---
+++ utils.js
@@ -0,0 +1,5 @@
+async function getCurrentTab() {
+    let queryOptions = { active: true, currentWindow: true};
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+}
