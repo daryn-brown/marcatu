e3683c5295d79df7e858c2df21556bf2df5464aa
daryn <darynnbrown@gmail.com>
2022-06-02 14:47:19 -0500

    added play and delete functions

--- utils.js
+++ utils.js
@@ -1,5 +1,9 @@
-async function getCurrentTab() {
-    let queryOptions = { active: true, currentWindow: true};
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
+export async function getActiveTabURL() {
+    const tabs = await chrome.tabs.query({
+        currentWindow: true,
+        active: true
+    });
+  
+    return tabs[0];
 }
+
